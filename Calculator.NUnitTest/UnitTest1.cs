a397f14282c2ed64a6fb7c7ac7e420896c92d8d8
DESKTOP-NUPROA5\Андрей <areikyn16@gmail.com>
2021-01-13 15:34:51 +0300

    NUnit test execute

--- Calculator.NUnitTest/UnitTest1.cs
+++ Calculator.NUnitTest/UnitTest1.cs
@@ -3,6 +3,8 @@ using Calculator.Operation;
 using calculator.Models.DataValue;
 using System;
 using calculator.Logic;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Calculator.NUnitTest
 {
@@ -130,8 +132,130 @@ namespace Calculator.NUnitTest
             //act 
             //assert            
             Assert.Throws<InvalidOperandException>(() => Parsing.Operation(incomingDataOperands));
+        }        
+
+        [Test]
+        public void Type25addDouble()
+        {
+            var a = new DataValue();
+            //arrange             
+            string incomingData = "25";
+
+            //act 
+            //assert
+            Assert.That(a.firstValue = Parsing.Value(incomingData), Is.TypeOf(typeof(double)));
         }
 
+        [Test]
+        public void TypeRandomDataaddDouble()
+        {
+            Random rnd = new Random();
+            var a = new DataValue();            
+            double b = 15;
+            for (int i = 0; i < b; i++)
+            {
+                double c = rnd.Next(20, 100);                
+                string incomingData = c.ToString();
+                Assert.That(a.firstValue = Parsing.Value(incomingData), Is.TypeOf(typeof(double)));
+            }                    
+        }       
+
+        [Test]
+        public void Sub_Range1and150Subtraction([Range(1, 100)] int x)
+        {
+            var a = new DataValue();
+            var subtraction = new Subtraction(a);
+                                    
+            for (int i = 0; i < x; i++)
+            {
+                //arrange            
+                a.firstValue = x;
+                a.secondValue = 150;
+                double expected = a.firstValue - 150;
+
+                //act
+                double result = subtraction.Execute();
+
+                //assert
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [Test]
+        public void Sub_Range1and150Addition([Range(1,100)]int x)
+        {
+            var a = new DataValue();
+            var addition = new Addition(a);
+            for (int i = 0; i < x; i++)
+            {
+                //arrange            
+                a.firstValue = x;
+                a.secondValue = 150;
+                double expected = a.firstValue + 150;
+
+                //act
+                double result = addition.Execute();
+
+                //assert
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [Test]
+        public void Sub_Range1and150Multiplication([Range(1, 100)] int x)
+        {
+            var a = new DataValue();
+            var multiplication = new Multiplication(a);
+            for (int i = 0; i < x; i++)
+            {
+                //arrange            
+                a.firstValue = x;
+                a.secondValue = 150;
+                double expected = a.firstValue * 150;
+
+                //act
+                double result = multiplication.Execute();
+
+                //assert
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [Test]
+        public void Sub_Range0and150Division([Range(0, 100)] int x)
+        {
+            var a = new DataValue();
+            var division = new Division(a);
+            for (int i = 0; i < x; i++)
+            {
+                //arrange            
+                a.firstValue = 150;
+                a.secondValue = x;
+                double expected = 150 / a.secondValue;
+
+                //act
+                double result = division.Execute();
+
+                //assert
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [Test]
+        public void Sub_Range0and150DivisionDivided([Range(0, 100)] int x)
+        {
+            var a = new DataValue();
+            var division = new Division(a);
+            for (int i = 0; i < x; i++)
+            {
+                //arrange            
+                a.firstValue = 150;
+                a.secondValue = x;
+
+                //assert
+                Assert.Throws<DivideByZeroException>(() => division.Execute());
+            }
+        }
 
     }
 }
